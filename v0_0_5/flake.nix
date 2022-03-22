010788b156add5f26c5474db910d4ecd820194d2
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:17 -0300

    chore: re-index ../nimpkgs/c/cirru_parser/v0_0_5

--- v0_0_5/flake.nix
+++ v0_0_5/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-cirru_parser-v0_0_5.flake = false;
   inputs.src-cirru_parser-v0_0_5.owner = "Cirru";
-  inputs.src-cirru_parser-v0_0_5.ref   = "refs/tags/v0.0.5";
+  inputs.src-cirru_parser-v0_0_5.ref   = "v0_0_5";
   inputs.src-cirru_parser-v0_0_5.repo  = "parser.nim";
   inputs.src-cirru_parser-v0_0_5.type  = "github";
   
